aff41c278ac771c015b37be7c56f587ccdc71b37
DESKTOP-HV189S3\Egor <Egor.Krechun@indusoft.ru>
2024-12-13 17:50:12 +0700

    added http lib, format executor code

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,8 @@
+import http.HttpResponse;
+import promises.Promise;
+import http.HttpError;
+import queues.QueueFactory;
+import http.HttpClient;
 import haxe.ui.util.Variant;
 import haxe.ui.assets.ImageInfo;
 import haxe.ui.loaders.image.ImageLoader;
@@ -18,6 +23,8 @@ import ecs.Entity;
 class Main{
     static var friends:SynchronizedArray<FriendModel>;
     var menuView:MenuView;
+    var getFriendsExecutor:Executor;
+    var usersGetUrl = "http://localhost:5017/arnest/api/get-running-machine-checks-devices";//"http://jsonplaceholder.typicode.com/users";
     public function new() {
         trace("Start ecs things");
         
@@ -36,6 +43,7 @@ class Main{
         player.add(new PlayerComponent(player,resources));
         trace("End ecs things");
         var app = new HaxeUIApp();
+
         app.ready(function() {
             menuView = new MenuView();
             menuView.init(getFriendList);//,getFriendList);
@@ -48,10 +56,7 @@ class Main{
     }
     function gg():Void {
         var unused:hx.concurrent.Future.FutureCompletionListener<Void> = null;
-        var newfriends = new Array<FriendModel>();
-        newfriends.push(new FriendModel("qwe","https://placehold.co/32x32/orange/white/png","1"));
-        newfriends.push(new FriendModel("bvc","adq","2"));
-        newfriends.push(new FriendModel("zxc","bbb","3"));
+        /*
         for(friend in newfriends){
            //Sys.sleep(2);
            friends.add(friend);
@@ -61,7 +66,7 @@ class Main{
            menuView.setFriends(friends);
            
            
-        };
+        };*/
         friends = friends.map((friend:FriendModel)->{
         try{
             trace(friend.profileImageUrl);
@@ -81,73 +86,107 @@ class Main{
     });
     menuView.setFriends(friends);
      }
-    function getFriendList(userId:String){
-        var executor = Executor.create(3);  // <- 3 means to use a thread pool of 3 threads on platforms that support threads
-      // depending on the platform either a thread-based or timer-based implementation is returned
-      var unused:hx.concurrent.Future.FutureCompletionListener<Void> = null;
-      // define a function to be executed concurrently/async/scheduled (return type can also be Void)
-        var urls = ["https://placehold.co/32x32/orange/white/png","https://placehold.co/32x32/orange/green/png","https://placehold.co/32x32/blue/green/png"];
+
+    function createLoadImageTask(url:String):Void->Variant{
+        return ()->{
+            var imgData:Variant = null;
+            var compl:Bool = false;
+            ImageLoader.instance.load(url,(inf:ImageInfo)->{
+                if(inf != null && inf.data != null) { trace(inf.data); imgData = inf.data;}
+                trace("agagaga: "+imgData);
+                compl = true;
+            });
+            while(!compl) Sys.sleep(0.1);
+            return imgData;
+        }; 
+    }
+
+    function createLoadImageTasks(urls:Array<String>):Array<Void->Variant>{
+        var tasks:Array<Void->Variant> = new Array<Void->Variant>();
         for(url in urls){
             var task:Void->Variant = ()->{
                 var imgData:Variant = null;
                 var compl:Bool = false;
                 ImageLoader.instance.load(url,(inf:ImageInfo)->{
-                    
                     if(inf != null && inf.data != null) { trace(inf.data); imgData = inf.data;}
                     trace("agagaga: "+imgData);
                     compl = true;
                 });
-                while(!compl) Sys.sleep(0.2);
+                while(!compl) Sys.sleep(0.1);
                 return imgData;
             };
-            
-            var future = executor.submit(task);
-            future.onCompletion(result->{
-                switch(result) {
-                    case VALUE(value, time, _): {
-                        trace(value.toImageData());
-                        trace('Successfully execution at ${Date.fromTime(time)} with result: $value');
-                    }
-                    case FAILURE(ex, time, _):  trace('Execution failed at ${Date.fromTime(time)} with exception: $ex');
-                    case PENDING(_):      trace("Nothing is happening");
-                  }
-            });
-            switch (future.result) {
-                case VALUE(value, time, _): trace('Successfully execution at ${Date.fromTime(time)} with result: $value');
-                case FAILURE(ex, time, _):  trace('Execution failed at ${Date.fromTime(time)} with exception: $ex');
-                case PENDING(_):            trace("No result yet...");
-             }
+            tasks.push(task);
         }
+        return tasks;
+    }
+    function loadUsers():Promise<HttpResponse>{
+        var client = new HttpClient();
+        client.followRedirects = false; // defaults to true
+        //client.retryCount = 0; // defaults to 0
+        //client.retryDelayMs = 1000; // defaults to 1000
+       // client.provider = new MySuperHttpProvider(); // defaults to "DefaultHttpProvider"
+        client.requestQueue = QueueFactory.instance.createQueue(QueueFactory.SIMPLE_QUEUE); // defaults to "NonQueue"
+        //client.defaultRequestHeaders = ["someheader" => "somevalue"];
+        //client.requestTransformers = [new MyRequestTransformerA(), new MyRequestTransformerB()];
+        //client.responseTransformers = [new MyResponseTransformerA(), new MyResponseTransformerB()];
+        var promise = client.get(usersGetUrl,null,["Content-Type"=>"Application/json"]);/*.then(response -> {
+            var foo = response.bodyAsJson;
+            trace(foo);
+        }, (error:HttpError) -> {
+            // error
+            trace(error);
+        });*/
+        return promise;
+        /*Sys.sleep(4);
+        var newfriends = new Array<FriendModel>();
+        newfriends.push(new FriendModel("qwe","https://placehold.co/32x32/orange/white/png","1"));
+        newfriends.push(new FriendModel("bvc","adq","2"));
+        newfriends.push(new FriendModel("zxc","https://placehold.co/32x32/orange/green/png","3"));
+        return newfriends;*/
+    }
 
-      // submit 10 tasks each to be executed once asynchronously/concurrently as soon as possible
-      //for (i in 0...10) {
-         //executor.submit(gg);
-      //}
+    
+    function getFriendList(userId:String){
+        var unused:hx.concurrent.Future.FutureCompletionListener<Void> = null;
+        
 
-      /*executor.submit(myTask, ONCE(2000));            // async one-time execution with a delay of 2 seconds
-      executor.submit(myTask, FIXED_RATE(200));       // repeated async execution every 200ms
-      executor.submit(myTask, FIXED_DELAY(200));      // repeated async execution 200ms after the last execution
-      executor.submit(myTask, HOURLY(30));            // async execution 30min after each full hour
-      executor.submit(myTask, DAILY(3, 30));          // async execution daily at 3:30
-      executor.submit(myTask, WEEKLY(SUNDAY, 3, 30)); // async execution Sundays at 3:30
+        var client = new HttpClient();
+        client.followRedirects = false;
+        client.requestQueue = QueueFactory.instance.createQueue(QueueFactory.SIMPLE_QUEUE);
+        var promise = client.get(usersGetUrl,null,["Content-Type"=>"Application/json"]);
 
-      // submit a task and keep a reference to it
-      var future = executor.submit(myTask, FIXED_RATE(200));
-*/
-      // check if a result is already available
-      /*switch (future.result) {
-         case VALUE(value, time, _): trace('Successfully execution at ${Date.fromTime(time)} with result: $value');
-         case FAILURE(ex, time, _):  trace('Execution failed at ${Date.fromTime(time)} with exception: $ex');
-         case PENDING(_):            trace("No result yet...");
-      }
+        promise.then(promiseResult->{
+            var foo = promiseResult.bodyAsJson;
+            getFriendsExecutor = Executor.create(3,true);  // <- 3 means to use a thread pool of 3 threads on platforms that support threads
+            var syncResult = new Array<FriendModel>();
+            syncResult.push(new FriendModel("qwe","https://placehold.co/32x32/orange/white/png","1"));
+            syncResult.push(new FriendModel("bvc","adq","2"));
+            syncResult.push(new FriendModel("zxc","https://placehold.co/32x32/orange/green/png","3"));
+            var users:SynchronizedArray<FriendModel> = new SynchronizedArray<FriendModel>();
+            for(user in syncResult){
+                users.add(user);
+            }
 
-      // check if the task is scheduled to be executed (again) in the future
-      if (!future.isStopped) {
-         trace('The task is scheduled for further executions with schedule: ${future.schedule}');
-      }
-
-      // cancel any future execution of the task
-      future.cancel();*/
+            for(user in users){
+                //friendMap.set(user.id,user.profileImageUrl);
+                var task = createLoadImageTask(user.profileImageUrl);
+                var future = getFriendsExecutor.submit(task);
+                var idx = users.indexOf(user);
+                future.onCompletion(result->{
+                    switch(result) {
+                        case VALUE(value, time, _): {
+                            trace(value.toImageData());
+                            trace('Successfully execution at ${Date.fromTime(time)} with result: $value');
+                            users[idx].image = value.toImageData();
+                            menuView.setFriends(users);
+                        }
+                        case FAILURE(ex, time, _):  trace('Execution failed at ${Date.fromTime(time)} with exception: $ex');
+                        case PENDING(_):      trace("Nothing is happening");
+                    }
+                });
+            }
+            menuView.setFriends(users);
+        });
     }
 
 
